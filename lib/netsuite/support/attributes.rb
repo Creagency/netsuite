9d2b0afeb96d71045dab4336c554a573bb2816cc
DawsonLeBrown <dawsonlebrown@gmail.com>
2020-07-09 15:04:09 +0200

    custom attributes from saved searches on records

--- lib/netsuite/support/attributes.rb
+++ lib/netsuite/support/attributes.rb
@@ -9,14 +9,28 @@ module NetSuite
       def attributes=(attributes)
         @attributes = attributes
       end
+      def initiation_attributes
+        @initiation_attributes ||= {}
+      end
+
+      def initiation_attributes=(initiation_attributes)
+        @initiation_attributes = initiation_attributes
+      end
 
       def initialize_from_attributes_hash(attributes = {})
+        self.initiation_attributes = attributes
         attributes.select { |k,v| self.class.fields.include?(k) }.each do |k,v|
           send("#{k}=", v)
         end
         self.klass = attributes[:class] if attributes[:class]
       end
-
+      def method_missing(m, *args, &block)
+        rv = initiation_attributes[m]
+        if rv.nil?
+          super 
+        end
+        return rv
+      end
     end
   end
 end
